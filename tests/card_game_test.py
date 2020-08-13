f3821c21a2bd49cf5dc63bf5964a1896ae10fe15
marekboro <marek.borowicz83@gmail.com>
2020-08-13 15:41:26 +0100

    last test added and works

--- tests/card_game_test.py
+++ tests/card_game_test.py
@@ -23,21 +23,25 @@ class CardGameTest(unittest.TestCase):
 
     def test_card_value(self):
         actual = self.card1.value
-        expected = 1
-        expected = 2
+        expected = 1                                # to fail test
+        expected = 2                                # comment out current line to have the previous one fail test.
         self.assertEqual(expected,actual)
 
     def test_check_for_ace_method(self):
         actual = CardGame.checkforAce(self,self.ace_of_diamonds)
-        expected = False
-        expected = True
+        expected = False                            # to fail test
+        expected = True                             # comment out current line to have the previous one fail test.
         self.assertEqual(expected,actual)
     
     def test_highest_card(self):
         actual = CardGame.highest_card(self,self.ace_of_diamonds,self.card1)
-        expected = Card(self.suit3,1)
-        expected = self.card1
+        expected = Card(self.suit3,1)               # to fail test
+        expected = self.card1                       # comment out current line to have the previous one fail test.
         self.assertEqual(expected,actual)
 
-
+    def test_cards_total(self):
+        actual = CardGame.cards_total(self,self.cards)
+        expected = "You have a total of 1"          # to fail test
+        expected = "You have a total of 3"          # comment out current line to have the previous one fail test.
+        self.assertEqual(expected,actual)
     
